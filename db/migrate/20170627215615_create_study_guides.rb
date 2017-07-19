804a752158766defbe01e6f14c17ee06861a4769
antciccone <aiccone2014@gmail.com>
2017-07-19 17:12:26 -0400

    take out migration

--- db/migrate/20170627215615_create_study_guides.rb
+++
@@ -1,9 +0,0 @@
-class CreateStudyGuides < ActiveRecord::Migration[5.0]
-  def change
-    create_table :study_guides do |t|
-      t.references :user, foreign_key: true
-      t.attachment :avatar
-      t.timestamps
-    end
-  end
-end
